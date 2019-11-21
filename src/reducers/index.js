ba76f6c0371ea4c610c58dcb9a86dd18ea360564
prr <Mr.gustav009@gmail.com>
2019-11-21 23:56:30 +0300

    Логика добавления книг в cart-table

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,43 @@
 const initialState = {
 	books: [],
 	loading: true,
-	error: null
+	error: null,
+	cartItems: []
+}
+
+const updateCartItems = (indexId, newBook, cartItems) => {
+	if (indexId === -1) {
+		return [ ...cartItems, newBook ]
+	}
+
+	return [
+			...cartItems.slice(0,indexId),
+			newBook,
+			...cartItems.slice(indexId + 1) 
+		]
+}
+
+const updateBookItem = ( book, item = {} ) => {
+
+	const { 
+		id = book.id,
+		count = 0,
+		total = 0,
+		title = book.title } = item
+	
+	return {
+		id,
+		title,
+		count: count + 1,
+		total: total + book.total
+	}
 }
 
 const reducer = (state = initialState, action) => {
 	switch (action.type){
 		case 'FETCH_BOOKS_REQUEST':
 			return {
+				...state,
 				books: [],
 				loading: true,
 				error: null
@@ -15,6 +45,7 @@ const reducer = (state = initialState, action) => {
 
 		case 'FETCH_BOOKS_SUCCESS':
 			return {
+				...state,
 				books: action.payload,
 				loading: false,
 				error: null
@@ -22,9 +53,25 @@ const reducer = (state = initialState, action) => {
 
 		case 'FETCH_BOOKS_FAILURE':
 			return {
+				...state,
 				books: [],
 				loading: false,
 				error: action.payload
+			}
+
+		case 'ADDED_TO_CART':
+			const bookId = action.payload;
+			const book = state.books.find(book => book.id === bookId);
+
+			const indexId = state.cartItems.findIndex(({ id }) => id === bookId)
+			const item = state.cartItems[indexId];
+
+			const newBook = updateBookItem(book, item)
+			const newBooks = updateCartItems(indexId, newBook, state.cartItems)
+
+			return {
+				...state,
+				cartItems: newBooks
 			}
 
 		default:
--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -5,13 +5,13 @@ export default class BookstoreService {
         id: 1,
         title: 'Production-Ready Microservices',
         author: 'Susan J. Fowler',
-        price: 32,
+        total: 32,
         coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg'},
       {
         id: 2,
         title: 'Release It!',
         author: 'Michael T. Nygard',
-        price: 45,
+        total: 45,
         coverImage: 'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg'}
     ];
 
