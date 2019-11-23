60938453dd67bcb4982b10fb1cfc1a44a5847958
prr <Mr.gustav009@gmail.com>
2019-11-23 17:10:52 +0300

    Middleware функции

--- src/store.js
+++ src/store.js
@@ -1,7 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const logMiddleware = () => (next) => (action) => {
+	console.log(action)
+	return next(action);
+}
+
+const stringMiddleware = state => next => action => {
+	if (typeof action === 'string') {
+		return next({type: action})
+	}
+	return next(action);
+}
+
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
+
+store.dispatch('Hi there!');
 
 export default store; 
 
